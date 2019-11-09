f010873ba6b3a5b68c1db59bd3ab0065edbce843
HojouFotytu <36724681+HojouFotytu@users.noreply.github.com>
2019-11-09 23:56:13 +0900

    update version to 2.2.10

--- PTMagic/Program.cs
+++ PTMagic/Program.cs
@@ -7,7 +7,7 @@ using Core.Helper;
 using Core.Main.DataObjects.PTMagicData;
 using Microsoft.Extensions.DependencyInjection;
 
-[assembly: AssemblyVersion("2.2.9")]
+[assembly: AssemblyVersion("2.2.10")]
 [assembly: AssemblyProduct("PT Magic")]
 
 namespace PTMagic
