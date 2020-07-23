29e1870329740199cc48accc4141f8502af66d8f
djbadders <34887832+djbadders@users.noreply.github.com>
2020-07-23 19:25:43 +0100

    Exposed new properties data correctly + fixed UI

--- Monitor/Pages/_get/DashboardBottom.cshtml.cs
+++ Monitor/Pages/_get/DashboardBottom.cshtml.cs
@@ -132,6 +132,7 @@ namespace Monitor.Pages
     {
       int tradeDayIndex = 0;
       string profitPerDayJSON = "";
+      
       if (PTData.SellLog.Count > 0)
       {
         DateTime minSellLogDate = PTData.SellLog.OrderBy(sl => sl.SoldDate).First().SoldDate.Date;
